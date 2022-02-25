5bb1da644da15f4c38fb6cd20741ce4122f0b9a1
miller.katlin <miller.katlin@gmail.com>
2022-02-24 22:16:21 -0700

    update Module

--- scraping.py
+++ scraping.py
@@ -13,17 +13,17 @@ def scrape_all():
     news_title, news_paragraph = mars_news(browser)
 
 # Run all scraping functions and store results in dictionary
-data = {
-    "news_title": news_title,
-    "news_paragraph": news_paragraph,
-    "featured_image": featured_image(browser),
-    "facts": mars_facts(),
-    "last_modified": dt.datetime.now()
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
 }
 
 # Stop webdriver and return data
-browser.quit()
-return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
 
